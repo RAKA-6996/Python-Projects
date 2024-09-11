c89eab73d0a3e08d47c5e2b11b4c5ee370719de6
RAKA-6996 <168769855+RAKA-6996@users.noreply.github.com>
2024-09-10 22:33:59 -0400

    Edit

--- SnakeWaterGun.py
+++ SnakeWaterGun.py
@@ -15,22 +15,38 @@ def computer_choice():
     return random.choice(['S','W','G']) # Computer chooses from the list of options
 
 def main():
-    player_choice = input("Choose:\nS for Snake\nW for Water\nG for Gun\nEnter: ").upper() # Player input
 
-    if player_choice not in choices: # Checks whether input is valid or not
-        print("Invalid input! Select from the options given.")
-        return
+    for i in range(3):
+    
+        player_choice = input("\nChoose:\nS for Snake\nW for Water\nG for Gun\nEnter: ").upper() # Player input
 
-    computer = computer_choice() # Stores the computer choice in the form of variable
-    print(f"Computer chose: {computer}")
+        if player_choice not in choices: # Checks whether input is valid or not
+            print("\nInvalid input! Select from the options given.")
+            return
 
-    result = main_matrix[choices[player_choice]][choices[computer]] # Evaluates the choice's dictionary and then search for exact indices in the main_matrix
+        computer = computer_choice() # Stores the computer choice in the form of variable
+        print(f"\nComputer chose: {computer}")
 
-    if result == 1: # Displays the result
-        print("You Won!")
-    elif result == 0:
-        print("Draw")
+        result = main_matrix[choices[player_choice]][choices[computer]] # Evaluates the choice's dictionary and then search for exact indices in the main_matrix
+
+        player_points = 0
+        computer_points = 0
+
+        if result == 1: # Displays the result
+            print("\nYou Won!")
+            player_points =+ 1
+        elif result == 0:
+            print("\nDraw")
+        else:
+            print("\nYou Lost")
+            computer_points =+ 1
+
+    if player_points > computer_points:
+        print("\nCongrats you won the game!!")
+    elif computer_points > player_points:
+        print("\nSorry you lost, try again.")
     else:
-        print("You Lost")
+        print("It is a Draw!")
+
 
 main()
