b5ce4d7bfbb63d9f5db1b4a7b5fbf7257afe6977
RAKA-6996 <168769855+RAKA-6996@users.noreply.github.com>
2024-09-21 12:28:32 -0400

    comments added

--- MatrixCalculator.py
+++ MatrixCalculator.py
@@ -7,9 +7,10 @@ import os
 # define functions class: 8 functions define
 # define visual class : 6 functions read and write
 
-matrix_1 = []
+matrix_1 = []  # Matrices for the opeartions as an empty list
 matrix_2 = []
 
+# Matrices to store the value of different operations
 result_add = [[0,0,0], [0,0,0], [0,0,0]]
 result_sub = [[0,0,0], [0,0,0], [0,0,0]]
 result_product = [[0,0,0], [0,0,0], [0,0,0]]
@@ -20,7 +21,8 @@ result_sqr = [[0,0,0], [0,0,0], [0,0,0]]
 class Functions:
     
     def matrix_01(self):
-        
+        ''' Takes in input from the user for 1st Matrix'''
+
         for i in range(1,4):
 
             row_element = []
@@ -33,29 +35,31 @@ class Functions:
             matrix_1.append(row_element)
             
     def matrix_02(self):
-        
+        ''' Takes in input from the user for 2nd Matrix'''
+
         for i in range(1,4):
 
             row_element = []
 
             for j in range(1,4):
 
-                elements = int(input(f"Enter {j}th element for row {i} in Matrix 2: "))
-                row_element.append(elements)
+                elements = int(input(f"Enter {j}th element for row {i} in Matrix 2: "))  # Takes input from the user for each element 
+                row_element.append(elements)  # Adds the elements into each row
 
-            matrix_2.append(row_element)
+            matrix_2.append(row_element)  # Adds the rows to Matrix
       
     def add_func(self):
+        ''' Add function to add 2 Matrix'''
 
-        self.matrix_01()
+        self.matrix_01() # Calling Matrices in this function to operate
         self.matrix_02()
         
         for i in range(0,3):
 
             for j in range(0,3):
 
-                result_add[i][j] = matrix_1[i][j] + matrix_2[i][j]
-                
+                result_add[i][j] = matrix_1[i][j] + matrix_2[i][j]  # This line adds the Matrix 1 and Matrix 2 and stores the result in result_add Matrix
+
         print('\n')
         
         for i in result_add:
@@ -65,6 +69,7 @@ class Functions:
         print("\nOPEN THE MATRIX VISUALS FILE FOR MORE DETAILS")
         
     def minus_func(self):
+        ''' Minus function to subtract 2 Matrix'''
 
         self.matrix_01()
         self.matrix_02()
@@ -84,6 +89,7 @@ class Functions:
         print("\nOPEN THE MATRIX VISUALS FILE FOR MORE DETAILS") 
         
     def product_func(self):
+        ''' Product function to multiply 2 Matrix'''
 
         self.matrix_01()
         self.matrix_02()
@@ -103,6 +109,7 @@ class Functions:
         print("\nOPEN THE MATRIX VISUALS FILE FOR MORE DETAILS")
         
     def div_func(self):
+        ''' Divide function to divide 2 Matrix'''
 
         self.matrix_01()
         self.matrix_02()
@@ -122,14 +129,15 @@ class Functions:
         print("\nOPEN THE MATRIX VISUALS FILE FOR MORE DETAILS")
          
     def trans_func(self):
+        ''' Transpose function to swap the rows and columns of one particular matrix'''
 
-        self.matrix_01()
+        self.matrix_01()  # Only Matrix 1 is called
         
         for i in range(0,3):
 
             for j in range(0,3):
 
-                result_trans[j][i] = matrix_1[i][j]
+                result_trans[j][i] = matrix_1[i][j]  # Interchanged the rows with columns and vice versa
                 
         print('\n')
         
@@ -140,6 +148,7 @@ class Functions:
         print("\nOPEN THE MATRIX VISUALS FILE FOR MORE DETAILS")
         
     def sqr_func(self):
+        ''' Square function to square each element of particular matrix'''
 
         self.matrix_01()
         
@@ -147,7 +156,7 @@ class Functions:
 
             for j in range(0,3):
 
-                result_sqr[i][j] = matrix_1[i][j] * matrix_1[i][j]
+                result_sqr[i][j] = matrix_1[i][j] * matrix_1[i][j]  # Mutliplying the matrix by itself
 
         print('\n')
 
@@ -157,17 +166,19 @@ class Functions:
             
         print("\nOPEN THE MATRIX VISUALS FILE FOR MORE DETAILS")
         
-obj1 = Functions()
+obj1 = Functions()  # Created the obj1 object of class Function()
 
-os.chdir("C:/Users/malay/Documents/Github/Python-Projects")
+os.chdir("C:/Users/malay/Documents/Github/Python-Projects")  # Directory changed
 
 class Visuals:
     
     def __init__(self, instance):
+        ''' Creating Constructor method to intialize object for this class and to access the Functions() class'''
         self.instance = instance
     
     def add_func_printer(self):
-        
+        ''' Prints the result of add operation in MatrixVisuals text file'''
+
         with open("MatrixVisuals.txt", 'w') as f:
             f.write("Matrix 1:\n")
 
@@ -176,6 +187,7 @@ class Visuals:
             f.write("\n")
             f.write("+\n")
             f.write("\n")
+
             f.write("Matrix 2:\n")
 
             for row in matrix_2:
@@ -183,6 +195,7 @@ class Visuals:
             f.write("\n")
             f.write("=\n")
             f.write("\n")
+
             f.write("Result:\n")
 
             for row in result_add:
@@ -190,7 +203,7 @@ class Visuals:
             f.write("\n")
             
     def sub_func_printer(self):
-        
+        ''' Prints the result of subtract operation in MatrixVisuals text file'''
         with open("MatrixVisuals.txt", 'w') as f:
             f.write("Matrix 1:\n")
 
@@ -199,6 +212,7 @@ class Visuals:
             f.write("\n")
             f.write("-\n")
             f.write("\n")
+
             f.write("Matrix 2:\n")
 
             for row in matrix_2:
@@ -206,6 +220,7 @@ class Visuals:
             f.write("\n")
             f.write("=\n")
             f.write("\n")
+
             f.write("Result:\n")
 
             for row in result_sub:
@@ -213,7 +228,8 @@ class Visuals:
             f.write("\n")
             
     def product_func_printer(self):
-        
+        ''' Prints the result of product function in MatrixVisuals text file'''
+
         with open("MatrixVisuals.txt", 'w') as f:
             f.write("Matrix 1:\n")
 
@@ -222,6 +238,7 @@ class Visuals:
             f.write("\n")
             f.write("*\n")
             f.write("\n")
+
             f.write("Matrix 2:\n")
 
             for row in matrix_2:
@@ -229,6 +246,7 @@ class Visuals:
             f.write("\n")
             f.write("=\n")
             f.write("\n")
+
             f.write("Result:\n")
 
             for row in result_product:
@@ -236,7 +254,8 @@ class Visuals:
             f.write("\n")
             
     def div_func_printer(self):
-        
+        '''Prints the divide result into the MatrixVisuals text file '''
+
         with open("MatrixVisuals.txt", 'w') as f:
             f.write("Matrix 1:\n")
 
@@ -245,6 +264,7 @@ class Visuals:
             f.write("\n")
             f.write("/\n")
             f.write("\n")
+
             f.write("Matrix 2:\n")
 
             for row in matrix_2:
@@ -252,6 +272,7 @@ class Visuals:
             f.write("\n")
             f.write("=\n")
             f.write("\n")
+
             f.write("Result:\n")
 
             for row in result_div:
@@ -259,7 +280,8 @@ class Visuals:
             f.write("\n")
             
     def trans_func_printer(self):
-        
+        ''' Prints the result of Transpose function into MatrixVisuals text file'''
+
         with open("MatrixVisuals.txt", 'w') as f:
             f.write("Matrix 1's Transpose:\n")
 
@@ -268,6 +290,7 @@ class Visuals:
             f.write("\n")
             f.write("=\n")
             f.write("\n")
+
             f.write("Result:\n")
 
             for row in result_trans:
@@ -275,7 +298,8 @@ class Visuals:
             f.write("\n")
             
     def sqr_func_printer(self):
-        
+        ''' Prints the result of square function into MatrixVisuals text file'''
+
         with open("MatrixVisuals.txt", 'w') as f:
             f.write("(Matrix 1)^2:\n")
 
@@ -284,25 +308,26 @@ class Visuals:
             f.write("\n")
             f.write("=\n")
             f.write("\n")
+
             f.write("Result:\n")
 
             for row in result_sqr:
                 f.write(f"{row}\n")
             f.write("\n")
             
-obj2 = Visuals(obj1)
+obj2 = Visuals(obj1)  # Created the obj2 object and used the obj1 as an instance to access the Function class in Visuals() class
 
 print("WELCOME TO MATRIX CALCULATOR\nPERFORM CALCULATION HERE AND GET OUTPUT IN A TEXT FILE!")
 
 user_input = int(input("Enter which operation do you want to perform:\nType 0 for Plus\nType 1 for Minus\nType 2 for Product\nType 3 for Division\nType 4 for Transpose\nType 5 for Squaring each element\nType 6 to Exit\nEnter Here: "))
 
-if user_input < 0 or user_input > 6:
+if user_input < 0 or user_input > 6:  # Setting the range of input for users
 
     raise IndexError("Out of Bound!! BAKCHODI MAT KAR LO*E")
     
 else:
 
-    match user_input:
+    match user_input:  # Switch Cases connected with user_input
             
         case 0:
             obj1.add_func()
